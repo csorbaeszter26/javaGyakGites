a40591fe63e6768f00b6b95254d1c12343c9d2e9
csorbaeszter26 <csorba.eszter.26@gmail.com>
2024-09-30 13:18:44 +0200

    komment nélkül

--- src/gitbrranch/GitBrranch.java
+++ src/gitbrranch/GitBrranch.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitbrranch;
 
-/**
- *
- * @author csorba.e.katalin
- */
+
 public class GitBrranch {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
